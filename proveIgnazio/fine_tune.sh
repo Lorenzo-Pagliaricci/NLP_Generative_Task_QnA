be1847cf08317eb30fbe0005b64648f245ffd718
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2025-04-06 18:56:21 +0200

    updated fine_tune.sh file

--- proveIgnazio/fine_tune.sh
+++ proveIgnazio/fine_tune.sh
@@ -1,10 +1,14 @@
+# --config proveIgnazio/data/dataset.yaml
 
 
+# huggingface-cli download mlx-community/gemma-3-1b-it-4bit  --local-dir ./models/base/gemma-3-1b-it-4bit
 
 mlx_lm.lora \
-    --model mlx-community/gemma-3-text-12b-it-4bit \
+    --model ./proveIgnazio/models/base/gemma-3-1b-it-4bit \
     --train \
-    --batch-size 20 \
-    --num-layers 32 \
-    --data data/processed \
-    --iters 60
+	--adapter-path ./models/adapters/adapter_gemma-3-1b-it-4bit \
+    --batch-size 1 \
+    --num-layers 4 \
+    --data proveIgnazio/data/processed \
+    --iters 500 \
+    --step-per-eval 10
