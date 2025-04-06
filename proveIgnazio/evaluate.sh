8de1b505e40b1da38bb753c52e1170d18e3820db
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2025-04-06 19:26:10 +0200

    fixed some problem with evaluate shell

--- proveIgnazio/evaluate.sh
+++ proveIgnazio/evaluate.sh
@@ -2,4 +2,8 @@ mlx_lm.lora \
     --model ./proveIgnazio/models/base/gemma-3-1b-it-4bit \
     --adapter-path ./proveIgnazio/models/adapters/adapter_gemma-3-1b-it-4bit \
     --data proveIgnazio/data/processed \
-    --test
+    --test 
+    # --batch-size 1 \
+    # --num-layers 4 \
+    # --iters 500 \
+    # --steps-per-eval 10
