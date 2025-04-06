c7022f376cfe617da8736dc08d17786e583c588f
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2025-04-06 19:11:49 +0200

    implemented evaluate.sh file

---
+++ proveIgnazio/evaluate.sh
@@ -0,0 +1,5 @@
+mlx_lm.lora \
+    --model ./proveIgnazio/models/base/gemma-3-1b-it-4bit \
+    --adapter-path ./proveIgnazio/models/adapters/adapter_gemma-3-1b-it-4bit \
+    --data proveIgnazio/data/processed \
+    --test
