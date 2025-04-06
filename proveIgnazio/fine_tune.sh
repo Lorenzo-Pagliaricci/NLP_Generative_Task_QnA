3fa47df720fb0f418f51a1e793689f87a81dc8c6
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2025-04-06 10:04:41 +0200

    updated fine tuning script

--- proveIgnazio/fine_tune.sh
+++ proveIgnazio/fine_tune.sh
@@ -1,7 +1,7 @@
 mlx_lm.lora \
     --model mlx-community/gemma-3-text-12b-it-4bit \
     --train \
-    --batch-size 1 \
-    --num-layers 4 \
-    --data data/processed/bioasq_qa.jsonl \
-    --iters 10 
+    --batch-size 20 \
+    --num-layers 32 \
+    --data data/processed \
+    --iters 60
