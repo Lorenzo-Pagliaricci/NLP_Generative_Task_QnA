0e7244b53f2a4b1e3dc537fa9dbdac35cea7911f
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2025-04-07 10:24:24 +0200

    added script for chatting and evaluation

---
+++ proveIgnazio/chat.sh
@@ -0,0 +1,4 @@
+mlx_lm.chat \
+    --model ./proveIgnazio/models/base/Llama-3.2-1B-Instruct-4bit \
+    --adapter-path ./proveIgnazio/models/adapters/adapter_Llama-3.2-1B-Instruct-4bit_2bs_4ls \
+    --max-tokens 1000
