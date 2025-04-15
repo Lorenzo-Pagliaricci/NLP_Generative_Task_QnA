77aaab909f2136243bb1e501c2a2821755c2044f
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2025-04-15 21:58:25 +0200

    renamed folder

--- proveIgnazio/chat.sh
+++
@@ -1,4 +0,0 @@
-mlx_lm.chat \
-    --model ./proveIgnazio/models/base/Phi-3-mini-4k-instruct-4bit \
-    --adapter-path ./proveIgnazio/models/adapters/adapter_Phi-3-mini-4k-instruct-4bit_2bs_4ls \
-    --max-tokens 1000
--- proveIgnazio/generate.sh
+++
@@ -1,5 +0,0 @@
-mlx_lm.generate \
-    --model ./proveIgnazio/models/base/Llama-3.2-1B-Instruct-4bit \
-	--adapter-path ./proveIgnazio/models/adapters/adapter_Llama-3.2-1B-Instruct-4bit_2bs_4ls \
-    --prompt "Is there any relationship between histone ubiquitylation and splicing?" \
-    --max-tokens 1000
