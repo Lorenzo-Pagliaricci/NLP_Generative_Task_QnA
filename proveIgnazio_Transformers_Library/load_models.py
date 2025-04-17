0f08df9810cc26ae5c58db532b13473d9dfeaf94
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2025-04-17 09:28:19 +0200

    implemented LoRA peft technique

--- proveIgnazio_Transformers_Library/load_models.py
+++ proveIgnazio_Transformers_Library/load_models.py
@@ -4,13 +4,17 @@ from transformers import TrainingArguments, Trainer, DataCollatorForSeq2Seq
 from dotenv import dotenv_values 
 from datasets import load_from_disk
 from metrics_utils import compute_metrics
+from peft import LoraConfig, TaskType, get_peft_model
+
+
 
 # --- Configuration ---
 # Load environment variables from the .env file located in the specified path
-config = dotenv_values("/proveIgnazio_Transformers_Library/.env")
+config = dotenv_values("proveIgnazio_Transformers_Library/.env")
 
 # --- Load Models ---
 # Get the model name/path from the loaded configuration
+MODEL_NAME = 'T5_SMALL_60M'
 MODEL = config["T5_SMALL_60M"]
 PREPARED_DATASET = config.get("TOKENIZED_DATASET", config["PREPARED_DATASET"])
 SAVED_MODEL_PATH = config["SAVED_MODEL_PATH"]
@@ -32,20 +36,25 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
 # --- Load Training Arguments ---
 training_args = TrainingArguments(
-    output_dir="/proveIgnazio_Transformers_Library/results", # Directory to save the model and training outputs
-    per_device_train_batch_size=1, # Batch size for training on each device
-    per_device_eval_batch_size=1, # Batch size for evaluation on each device
-    gradient_accumulation_steps=10, # Number of steps to accumulate gradients before updating weights
+    output_dir="proveIgnazio_Transformers_Library/results", # Directory to save the model and training outputs
+    per_device_train_batch_size=2, # Batch size for training on each device
+    per_device_eval_batch_size=2, # Batch size for evaluation on each device
+    gradient_accumulation_steps=5, # Number of steps to accumulate gradients before updating weights
     num_train_epochs=10, # Total number of training epochs
-    logging_dir="/proveIgnazio_Transformers_Library/logs", # Directory for storing logs
-    logging_steps=10, # Log every 10 steps
+    logging_dir=f"proveIgnazio_Transformers_Library/tensorboard_logs/{MODEL_NAME}", # Directory for storing logs
+    logging_steps=100, # Log every 10 steps
     save_steps=500, # Save the model every 500 steps
     evaluation_strategy="steps", # Evaluate the model every 'eval_steps'
+    do_eval=True, # Perform evaluation during training
     eval_steps=500, # Evaluate every 500 steps
     seed=42, # Random seed for reproducibility
     load_best_model_at_end=True, # Load the best model at the end of training
     metric_for_best_model="eval_loss", # Metric to determine the best model
     greater_is_better=False, # Whether a higher metric value is better
+    learning_rate=5e-5,  # Learning rate
+    warmup_steps=500, # Number of warmup steps for learning rate scheduler
+    eval_accumulation_steps=500, # Number of steps to accumulate gradients during evaluation
+    eval_on_start=True, # Evaluate at the start of training
 )
 
 # Load the dataset
@@ -55,6 +64,22 @@ print(f"Dataset structure: {dataset}")
 
 # Check if we're using a tokenized dataset or if we need to tokenize on-the-fly
 is_tokenized = all(col in dataset["train"].column_names for col in ["input_ids", "attention_mask", "labels"])
+
+
+# --- LoRA Configuration ---
+# Define the LoRA configuration for model adaptation
+lora_config = LoraConfig(
+    task_type=TaskType.SEQ_2_SEQ_LM, # type of task to train on
+    inference_mode=False, # set to False for training
+    r=8, # dimension of the smaller matrices
+    lora_alpha=32, # scaling factor
+    lora_dropout=0.1, # dropout of LoRA layers
+    # target_modules=["q", "k", "v", "o"] # Specify the target modules for LoRA adaptation
+)
+
+# Apply LoRA using get_peft_model
+model = get_peft_model(model, lora_config)
+
 
 # Create a data collator for padding sequences in batches efficiently
 data_collator = DataCollatorForSeq2Seq(
@@ -94,7 +119,7 @@ else:
         train_dataset=tokenized_datasets["train"],
         eval_dataset=tokenized_datasets["validation"],
         data_collator=data_collator,
-        compute_metrics=compute_metrics,
+        compute_metrics=compute_metrics,    
     )
 
 # --- Training ---
